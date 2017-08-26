08ef4e3141de54634db156d2dfd11652f2186749
Gurudatta Patekar <gurudatta.patekar@creativecapsule.com>
2017-08-26 20:15:21 +0530

    no message

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 
 import { AppComponent } from './app.component';
@@ -9,17 +10,22 @@ import { DiscussionComponent } from './pages/discussion/discussion.component';
 import { ArticleComponent } from './pages/article/article.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Sib1Component } from './data-transfer/sib1/sib1.component';
+import { Sib2Component } from './data-transfer/sib2/sib2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     DiscussionComponent,
-    ArticleComponent
+    ArticleComponent,
+    Sib1Component,
+    Sib2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
