1f53bc4c540fbde9659ce7d342e858aa8293265e
Gurudatta Patekar <gurudatta.patekar@creativecapsule.com>
2017-08-30 20:15:46 +0530

    3d carousel other widgets

--- angular 2/my-app/src/app/shared/carousel-3d/carousel-3d.component.ts
+++ angular 2/my-app/src/app/shared/carousel-3d/carousel-3d.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit, ElementRef, NgZone  } from '@angular/core';
+
+import 'assets/js/3d-carousel/FWDPage.js';
+import 'assets/js/3d-carousel/FWDSlidersMenu.js';
+import 'assets/js/3d-carousel/FWDSlidersMenuButton.js';
+import 'assets/js/3d-carousel/FWDSlider.js';
+import 'assets/js/3d-carousel/FWDFloatSlider.js';
+import 'assets/js/3d-carousel/FWDPageMenuButton.js';
+import 'assets/js/3d-carousel/FWDPageThumbs.js';
+import 'assets/js/3d-carousel/FWDPageThumb.js';
+import 'assets/js/3d-carousel/FWDPageSimpleButton.js';
+import 'assets/js/3d-carousel/FWDOnOffButton.js';
+import 'assets/js/3d-carousel/FWDPageComboBox.js';
+import 'assets/js/3d-carousel/FWDResetButton.js';
+
+
 import 'assets/js/3d-carousel/FWDUltimate3DCarousel.js';
 import carousel3d from 'assets/js/3d-carousel/init-3dcarousel.js';
 
